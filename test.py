70edf95e902985a103c855575faf9ea1fcc57a53
Ricardo José Garzón Arias <42324647+Glifos6089@users.noreply.github.com>
2020-10-10 19:32:21 -0500

    actualizacion

--- test.py
+++ test.py
@@ -1,24 +1,28 @@
 import subprocess
 import os
 import signal
+import threading
+import time
+import getch
+import sys
+import subprocess
 
-print('''
-1: Show
-2: Create
-3: Kill
-4: Ver pid ultimo proceso creado
-5: exit
-''')
 
-op = int(input())
+
 proc = None
 
-while op <= 5 or op > 0:
+def mostrarTareas():
+	os.system('clear')
+	x = subprocess.check_output("ps -e -o stat,user,cmd,pid | grep '^S\|^R'",shell=True)
+	x = x.decode('UTF-8')
+	print(x)
+
+def desplegarMenu(op):
 	if op == 1:
-		x = subprocess.check_output("ps -ely",shell=True)
-		x = x.decode('UTF-8')
-		print(x)
+		mostrarTareas()
 	elif op == 2:
+		result = subprocess.check_output('cut -d: -f1 /etc/passwd', shell=True)
+		print(str(result).split("\\n"))
 		x = str(input("Ingrese proceso: \n"))
 		proc = subprocess.Popen(x, shell=True)
 		print("Proceso creado ")
@@ -31,14 +35,20 @@ while op <= 5 or op > 0:
 			print(proc.pid)
 		else:
 			print("No se han creado procesos ")
-		
-	print('''
+	elif op == 5:
+		mostrarTareas()
+
+if __name__ == "__main__":
+	op = 1
+	while(op != 5):
+		print('''
 1: Show
 2: Create
 3: Kill
 4: Ver pid ultimo proceso creado
 5: exit
-	''')
-	op = int(input())
+		''')
+		op = int(input())
+		desplegarMenu(op)
 	
 
