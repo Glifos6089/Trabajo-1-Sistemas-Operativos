d6ec5d2a1389ae447efaa888cdf4a4702cbd7328
Ricardo José Garzón Arias <42324647+Glifos6089@users.noreply.github.com>
2020-10-10 23:33:35 -0500

    holaxd

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import os
 import signal
 import threading
 import time
-import getch
 import sys
 import subprocess
 
@@ -13,7 +12,7 @@ proc = None
 
 def mostrarTareas():
 	os.system('clear')
-	x = subprocess.check_output("ps -e -o stat,user,cmd,pid | grep '^S\|^R'",shell=True)
+	x = subprocess.check_output("ps -A -o stat,user,ppid,pid,cmd --cols 100",shell=True)
 	x = x.decode('UTF-8')
 	print(x)
 
@@ -21,22 +20,44 @@ def desplegarMenu(op):
 	if op == 1:
 		mostrarTareas()
 	elif op == 2:
+
+		# Get the real user ID 
+		# of the current process 
+		# using os.getuid() method 
+		uid = os.getuid() 
+		
+		# Print the real user ID 
+		# of the current process 
+		print("Real user ID of the current process:", uid) 
+		
+		# Set real user ID 
+		# of the current process 
+		# using os.setuid() method 
+		#uid = 1001
+		#os.setuid(uid) 
+		#print("Real user ID changed") 
+		
+		
+		# Print the real user ID 
+		# of the current process 
+		print("Real user ID of the current process:", os.getuid()) 
+
+		
 		result = subprocess.check_output('cut -d: -f1 /etc/passwd', shell=True)
 		print(str(result).split("\\n"))
 		x = str(input("Ingrese proceso: \n"))
-		proc = subprocess.Popen(x, shell=True)
+		proc = subprocess.Popen(x,shell=True,user=1001)
 		print("Proceso creado ")
 	elif op ==3:
 		x = int(input("Ingrese PID: \n"))
 		p = os.kill(x, signal.SIGKILL)
 		print("Eliminado ")
 	elif op == 4:
-		if proc:
+		print(os.setgid(123))
+		try:
 			print(proc.pid)
-		else:
+		except:
 			print("No se han creado procesos ")
-	elif op == 5:
-		mostrarTareas()
 
 if __name__ == "__main__":
 	op = 1
